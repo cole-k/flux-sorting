f776967ed1a21508bebdf5e5815c2dbe9b105104
Cole K <cole-k@users.noreply.github.com>
2023-04-20 21:58:47 -0700

    flux-snapshot auto commit (no message)

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ mod sort;
 fn main() {
     // let range = insertion_sort::make_range(0, 10);
     // insertion_sort::print_list(&range);
-    let some_numbers = vec!(5, 12, 5, 2, 3, 1, 9, 2, 10);
-    sort::print_list(&sort::insertion_sort(&some_numbers));
+    // let some_numbers = vec!(5, 12, 5, 2, 3, 1, 9, 2, 10);
+    // sort::print_list(&sort::insertion_sort(&some_numbers));
 }
