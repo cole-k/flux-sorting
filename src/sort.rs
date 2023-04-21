8295c1f45c37db1bb1d0c51e1841a93ded77f42b
Cole K <cole-k@users.noreply.github.com>
2023-04-20 22:32:18 -0700

    flux-snapshot auto commit (no message)

--- src/sort.rs
+++ src/sort.rs
@@ -62,6 +62,11 @@ fn merge(list1: SortedList, list2: SortedList) -> SortedList {
     }
 }
 
+#[flux::trusted]
+fn unsafe_head(slice: &[i32]) -> i32 {
+    slice[0]
+}
+
 #[flux::sig(fn(&[i32]) -> SortedList)]
 pub fn merge_sort(unsorted_slice: &[i32]) -> SortedList {
     if unsorted_slice.is_empty() {
@@ -70,7 +75,7 @@ pub fn merge_sort(unsorted_slice: &[i32]) -> SortedList {
     // Needed to prevent an infinite recursion when we split and get an empty
     // list and the same list.
     if unsorted_slice.len() == 1 {
-        return SortedList::Cons(unsorted_slice[0], Box::new(SortedList::Nil));
+        return SortedList::Cons(unsafe_head(unsorted_slice), Box::new(SortedList::Nil));
     }
     let (first_half, second_half) = unsorted_slice.split_at(unsorted_slice.len() / 2);
     merge(merge_sort(first_half), merge_sort(second_half))
