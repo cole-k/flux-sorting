2f7d8c0cdb841979e462a6b926af54e2ca1bdeea
Cole K <cole-k@users.noreply.github.com>
2023-04-20 23:57:53 -0700

    flux-snapshot auto commit (no message)

--- src/sort.rs
+++ src/sort.rs
@@ -146,7 +146,9 @@ pub fn merge_sort(unsorted_list: &List) -> SortedList {
     match unsorted_list {
         List::Nil => return SortedList::Nil,
         List::Cons(i, next) => {
-            return SortedList::Cons(*i, next);
+            if let List::Nil = **next {
+                return SortedList::Cons(*i, SortedList::Nil);
+            }
         }
     };
     let (l1, l2) = halve(unsorted_list);
