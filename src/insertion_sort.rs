65ed60c3f60d6bf8793bcd8d6ba6e82c9cc78ef8
Cole K <cole-k@users.noreply.github.com>
2023-04-20 19:27:05 -0700

    flux-snapshot auto commit (no message)

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -6,8 +6,14 @@ enum SortedList {
     Cons(u32, Box<SortedList>)
 }
 
-fn simple_test() {
+fn simple_test_ok() {
     let zero = SortedList::Cons(0, Box::new(SortedList::Nil));
     let one = SortedList::Cons(1, Box::new(zero));
     let two = SortedList::Cons(2, Box::new(one));
+}
+
+fn simple_test_fail() {
+    let zero = SortedList::Cons(0, Box::new(SortedList::Nil));
+    let two = SortedList::Cons(2, Box::new(zero));
+    let one = SortedList::Cons(1, Box::new(one));
 }
