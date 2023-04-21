99a28dd9eecd38f603574f3d49490deb42cf4ec1
Cole K <cole-k@users.noreply.github.com>
2023-04-20 19:26:04 -0700

    flux-snapshot auto commit (no message)

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -2,6 +2,12 @@
 enum SortedList {
     #[flux::variant(SortedList[0])]
     Nil,
-    #[flux::variant((u32[@k], Box<SortedList{v: v <= k}>) -> SortedList[0])]
+    #[flux::variant((u32[@k], Box<SortedList{v: v <= k}>) -> SortedList[k])]
     Cons(u32, Box<SortedList>)
+}
+
+fn simple_test() {
+    let zero = Cons(0, Box::new(Nil));
+    let one = Cons(1, Box::new(zero));
+    let two = Cons(2, Box::new(one));
 }
