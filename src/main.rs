fb4118054112b7d07cf4b9529cd65e81d31c3ee3
Cole K <cole-k@users.noreply.github.com>
2023-04-20 22:12:27 -0700

    flux-snapshot auto commit (no message)

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@
 
 mod sort;
 
+#[flux::trusted]
 fn main() {
     // let range = insertion_sort::make_range(0, 10);
     // insertion_sort::print_list(&range);
-    // let some_numbers = vec!(5, 12, 5, 2, 3, 1, 9, 2, 10);
-    // sort::print_list(&sort::insertion_sort(&some_numbers));
+    let some_numbers = vec!(5, 12, 5, 2, 3, 1, 9, 2, 10);
+    sort::print_list(&sort::insertion_sort(&some_numbers));
+    sort::print_list(&sort::merge_sort(&some_numbers));
 }
--- src/sort.rs
+++ src/sort.rs
@@ -67,6 +67,11 @@ pub fn merge_sort(unsorted_slice: &[i32]) -> SortedList {
     if unsorted_slice.is_empty() {
         return SortedList::Nil;
     }
+    // Needed to prevent an infinite recursion when we split and get an empty
+    // list and the same list.
+    if unsorted_slice.len() == 1 {
+        return SortedList::Cons(unsorted_slice[0], Box::new(SortedList::Nil));
+    }
     let (first_half, second_half) = unsorted_slice.split_at(unsorted_slice.len() / 2);
     merge(merge_sort(first_half), merge_sort(second_half))
 }
